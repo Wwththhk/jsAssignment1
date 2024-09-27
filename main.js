ff8baa4903f01d38d8cf97f8255fc69914e297b3
khk99 <khk990815@gmail.com>
2024-09-27 14:39:12 +0900

    상품들을 html에서 출력

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ let products = [
     new Product('배꼽살', 4000),
     new Product('중뱃살', 1000)
 ]
+
+let productContainer = document.getElementById('productContainer')
+
+products.forEach(product => {
+    let productItem = document.createElement('div')
+    productItem.className = 'product-item'
+    productItem.textContent = `${product.name} - ${product.price}`
+    productContainer.appendChild(productItem)
+})
